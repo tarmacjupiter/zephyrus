7e2c18c072ea81aa6e76ef9e49c49687620c4e7c
Upanshu <uparkh23@gmail.com>
2023-04-15 13:26:27 -0500

    Added basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
 
 function Footer(){
-    return (
-        <div>
-            <h1>Footer!</h1>
-        </div>
-    )
+  return (
+    <Box>
+      <h1 style={{ color: "white", 
+                   textAlign: "center", 
+                   marginTop: "-50px",
+                   marginLeft: "0px",
+                   width: "90%",
+                   padding: "10px"
+                   }}>
+        Project Zephyrus
+      </h1>
+      <Container>
+        {/* <Row> */}
+          <Column>
+            <Heading>GitHub Repo Link</Heading>
+            <FooterLink href="https://github.com/tarmacjupiter/zephyrus">
+              https://github.com/tarmacjupiter/zephyrus
+            </FooterLink>
+          </Column>
+        {/* </Row> */}
+      </Container>
+    </Box>
+  );
 }
 
 export default Footer;
